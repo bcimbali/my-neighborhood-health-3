62b6c02db2f4be46c9d0777e44ba5d63ef85982e
Brett Cimbalik <bcimbali@gmail.com>
2018-07-08 17:41:44 -0500

    updated routes folder

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+// const articleRoutes = require("./articles");
+// const nytRoutes = require("./nyt");
+
+// NYT routes - Imported from React hw
+// router.use("/articles", articleRoutes);
+
+// router.use("/nyt", nytRoutes);
+
+module.exports = router;
