33e83d5540ad18d84ab8397fcc2940334be9fe99
Brett Cimbalik <bcimbali@gmail.com>
2018-07-08 17:30:41 -0500

    added heroku/production info to server.js

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/my-neighborhood
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
+// Heroku will use this for production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view.
+app.use(routes);
 
 //start the API server
 app.listen(PORT, function() {
