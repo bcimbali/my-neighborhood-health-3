59e00afe9d49a151f67077ac6617de545ceebcd3
kglibrarian <karen.gutzman@gmail.com>
2018-07-11 20:05:16 -0500

    working on authentication

--- server.js
+++ server.js
@@ -5,9 +5,21 @@ const routes = require("./routes");
 const mongoose = require('mongoose');
 const favicon = require('express-favicon');
 const PORT = process.env.PORT || 3001; 
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 //connect to mongoose
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/my-neighborhood-health');
+
+//use sessions for tracking logins
+app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db
+    })
+  }));
 
 //define middleware
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -17,6 +29,9 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+// serve static files from template
+app.use(express.static(__dirname + '/templateLogReg'));
 
 // Add routes, both API and view.
 app.use(routes);
