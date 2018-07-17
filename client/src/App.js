4d1e9ba5532fd43a990fa23df96c79bd5b310845
Alejandra Rubalcava <arubalcava@group.on>
2018-07-16 19:37:32 -0500

    added nav

--- client/src/App.js
+++ client/src/App.js
@@ -8,33 +8,24 @@ import GoogleApiWrapper from './components/Map/map';
 import Authentication from './Pages/Authentication';
 
 
-<<<<<<< HEAD
-const App = () => (
-  <Router>
-    <div>
-      <Nav />
-      <Route exact path="/communities" component={Communities} />
-      <Route exact path="/news" component={News} />
-    </div>
-  </Router>
-);
-=======
 class App extends Component {
   render() {
     return (
 
       <Router>
         <div>
+          <Nav />
+          <Route exact path="/communities" component={Communities} />
+          <Route exact path="/news" component={News} />
           <Switch>
-            <Route exact path='/authentication' component = {Authentication} />
-            <Route exact path='/' component = {GoogleApiWrapper} /> 
+            <Route exact path='/authentication' component={Authentication} />
+            <Route exact path='/' component={GoogleApiWrapper} />
           </Switch>
         </div>
-    </Router>
-      
+      </Router>
+
     );
   }
 }
->>>>>>> 2a93e180d40901d733406ce095d9ce21c4f3e824
 
 export default App;
