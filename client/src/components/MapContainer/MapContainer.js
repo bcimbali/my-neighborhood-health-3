40a3f3938c0875a6b91b1f1ea7a03fe6cee8769d
Brett Cimbalik <bcimbali@gmail.com>
2018-07-17 21:47:47 -0500

    updated map.js file and names

--- client/src/components/MapContainer/MapContainer.js
+++ client/src/components/MapContainer/MapContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from '../../../../../../Library/Caches/typescript/2.9/node_modules/@types/react';
 // import {GoogleApiWrapper} from 'google-maps-react';
 import {InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import dummyData from "../../dummyData.json";
