29477e768fd4fe8736e11cb8d09393ee08d57f28
Alejandra Rubalcava <arubalcava@group.on>
2018-07-18 21:22:56 -0500

    news scraper works

---
+++ client/src/components/Article/Article.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Article = ({ title, link}) => (
+    <h3>
+      <em>{title}</em>{" "}
+      <span className="btn-group pull-right">
+        <a
+          className="btn btn-light"
+          href={link}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+        View Article
+        </a>
+      </span>
+    </h3>
+);
+
+export default Article;
