ee55eb570b7f24d81f9865ed2a75b70f6e542446
Alejandra Rubalcava <arubalcava@group.on>
2018-07-21 14:12:28 -0500

    created more community components

--- client/src/Pages/Communities/Communities.js
+++ client/src/Pages/Communities/Communities.js
@@ -1,39 +1,12 @@
 import React, {Component} from "react";
-import Post from '../../components/Post';
 import "./Communities.css";
-import PostEditor from '../PostEditor';
+import ThreadDisplay from "../ThreadDisplay";
+
 
 class Communities extends Component {
-    constructor(props) {
-        super(props);
-
-        this.addPost = this.addPost.bind(this);
-
-
-        this.state = {
-            posts: [],
-        }
-    }
-
-    addPost(newBodyPost) {
-        const newState = Object.assign({}, this.state);
-        newState.posts.push(newBodyPost);
-        this.setState(newState);
-    }
-
-
     render() {
         return (
-            <div>
-                {
-                    this.state.posts.map((postBody, idx) => {
-                        return (
-                            <Post idx={idx} postBody={postBody}/>
-                        )
-                    })
-                }
-                <PostEditor addPost={this.addPost} />
-            </div>
+            <ThreadDisplay />
         );
     }
 }
